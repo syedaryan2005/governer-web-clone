ba8f14450190d00dc176f3df5ed63533ed4be434
42501w <syedaryana869@gmail.com>
2025-05-05 02:34:23 +0500

    main

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-
+"use client"
 import Image from "next/image";
 import { MdOutlineMail } from "react-icons/md";
 
---
+++ types/react-icons.d.ts
@@ -0,0 +1,5 @@
+declare module 'react-icons/md' {
+  import { IconType } from 'react-icons';
+  export const MdOutlineMail: IconType;
+  // Add other icons you're using from md here
+} 
